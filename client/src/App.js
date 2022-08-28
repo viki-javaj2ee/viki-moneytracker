f95a7636db4f787c0eb349e7da4683a3ab7cad2a
Viki Sangani <viki.javaj2ee@gamil.com>
2022-08-28 16:57:40 +0100

    added dependencies for UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Viki Money Tracker</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
